9e39422b7b6b29dc12f0d9c1fe0e37318e34dfcb
Lindsey Kuper <lindsey@composition.al>
2016-08-05 16:52:33 -0700

    Comment out the download bit for now; remove existing .json files

--- download_and_json_all_problems.sh
+++ download_and_json_all_problems.sh
@@ -2,7 +2,9 @@
 
 ## Download all problems. Turn them all into json. OH YEAH.
 
-python3 download_problems.py
+#python3 download_problems.py
+
+rm problems/*.json
 
 for problem in problems/*; do
   echo $problem;
