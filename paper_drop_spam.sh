7f6fb952ca796b9ec336080af6b3f479aa893314
Alex Rudnick <alex.rudnick@gmail.com>
2016-08-05 19:43:07 -0700

    make paper_drop_spam a little more general, to work for user-submitted problems too

--- paper_drop_spam.sh
+++ paper_drop_spam.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-for i in `seq 1 101`; do
-  python3 paper_drop_solution.py problems/problem_$(printf "%03d\n" $i) \
+for i in `seq 1 2000`; do
+  problemfile="problems/problem_$(printf "%03d\n" $i)"
+
+  if [ ! -e $problemfile ]; then
+    continue
+  fi
+  echo "doing $i"
+
+  python3 paper_drop_solution.py $problemfile \
   > /tmp/paperdropsolution
   ./submit_solution $i /tmp/paperdropsolution
   sleep 1.5s
