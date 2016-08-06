d0984f9d26cc3a2d5760062f0166227543fe049a
Alex Rudnick <alex.rudnick@gmail.com>
2016-08-06 12:45:24 -0700

    starting on bounding-box generator with folding

---
+++ hv_folds.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+from collections import namedtuple
+
+from fractions import Fraction
+
+## NB: vfolds reduce width.
+## (they are like vsplits in vim)
+## ALSO NB: hfolds reduce height.
+
+## parts of a solution
+## - source point locations
+## - facets
+## - target locations
+
+## Every point has a source and target location.
+
+def count_folds(width, height):
+    """Given a bounding box of w, h, return the number of vfolds and hfolds to
+    contain that."""
+    return 2, 2
+
+
+## each of source and dest is an x,y coordinate. Always use fractions.
+## TYPE SAFETY AHOY.
+SolutionPoint = namedtuple("SolutionPoint", ["source", "dest"])
+
+def solution_for(vfolds, hfolds):
+    """Create a Solution for if we want to fold horizontally hfolds times and
+    vertically vfolds times."""
+
+    ncolumns = (2 ** vfolds) 
+    nrows = (2 ** hfolds) 
+
+    ## How many points?
+    npoints = ((2 ** hfolds) + 1) * ((2 ** vfolds) + 1)
+
+    ## how many facets?
+    nfacets = ncolumns * nrows
+
+    print("this many facets & points", nfacets, npoints)
+
+    solution_points = []
+
+    ## For each point, find its source location.
+    x_denominator = ncolumns
+    y_denominator = nrows
+    for y_numerator in range(nrows + 1):
+        for x_numerator in range(ncolumns + 1):
+            solution_points.append(
+                SolutionPoint((Fraction(x_numerator, x_denominator),
+                               Fraction(y_numerator, y_denominator)),
+                              "hello") )
+
+    solution_facets = []
+    ## for every point, add a facet unless you're on the right edge or on the
+    ## top edge.
+    for index, point in enumerate(solution_points):
+        right_edge = (point.source[0] == 1)
+        top_edge = (point.source[1] == 1)
+        if (right_edge or top_edge): continue
+
+        ## Don't stop 'til you get enough.
+        if len(solution_facets) == nfacets: break
+
+        solution_facets.append((index, index+1,
+                               index + ncolumns + 1 + 1, index + ncolumns + 1))
+
+    return solution_points, solution_facets
+
+def main():
+    print(solution_for(0, 0))
+    print()
+    print(solution_for(1, 1))
+    print()
+    print(solution_for(3, 2))
+
+if __name__ == "__main__":
+    main()
