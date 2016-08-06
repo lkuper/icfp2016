7a8db3f91badd6f5827a3cc2f4268c3c99d29ef5
Lindsey Kuper <lindsey@composition.al>
2016-08-05 20:39:32 -0700

    Loop through all problems we know of; check if each exists.

--- fold_in_half_spam.sh
+++ fold_in_half_spam.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-for i in `seq 1 101`; do
-  python3 fold_in_half_solution.py problems/problem_$(printf "%03d\n" $i) \
-  > /tmp/foldinhalfsolution
-  ./submit_solution $i /tmp/foldinhalfsolution
-  sleep 1.5s
+for i in `seq 1 1481`; do
+    if [ -e "problems/problem_$(printf "%03d\n" $i)" ]
+    then
+        python3 fold_in_half_solution.py problems/problem_$(printf "%03d\n" $i) \
+        > /tmp/foldinhalfsolution
+        ./submit_solution $i /tmp/foldinhalfsolution
+        sleep 1.5s
+    fi
 done
